62537d3cea53ffc800cc005067e3f792a11eff3e
Đặng Tân <tanwihwork@gmail.com>
2025-08-31 23:17:57 +0700

    fix cd

--- AuthenticationService/src/main/java/com/BookStore/AuthenticationService/controller/test.java
+++ AuthenticationService/src/main/java/com/BookStore/AuthenticationService/controller/test.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class test {
     @GetMapping("/test")
     public String test() {
-        return "Hello World";
+        return "Hello World1";
     }
 }
